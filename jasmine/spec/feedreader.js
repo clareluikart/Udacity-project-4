dd021edbcc39c372aa2946518f9071d66f6687ba
Clare Rickard <clareluikart@gmail.com>
2018-09-27 16:27:48 -0700

    Added final test and removed TODOs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
     });
 
 
-    /* TODO: Write a test that loops through each feed
+    /* test that loops through each feed
      * in the allFeeds object and ensures it has a URL defined
      * and that the URL is not empty.
      */
@@ -39,7 +39,7 @@ $(function() {
     });
 
 
-    /* TODO: Write a test that loops through each feed
+    /* test that loops through each feed
      * in the allFeeds object and ensures it has a name defined
      * and that the name is not empty.
      */
@@ -54,9 +54,9 @@ $(function() {
 
 
 
-  /* TODO: Write a new test suite named "The menu" */
+  /* test suite named "The menu" */
   describe('The menu', function() {
-    /* TODO: Write a test that ensures the menu element is
+    /* a test that ensures the menu element is
      * hidden by default. You'll have to analyze the HTML and
      * the CSS to determine how we're performing the
      * hiding/showing of the menu element.
@@ -64,7 +64,7 @@ $(function() {
     it('should be hidden by default', function() {
       expect(document.getElementsByTagName('body')[0].classList[0]).toBe('menu-hidden');
     });
-    /* TODO: Write a test that ensures the menu changes
+    /* a test that ensures the menu changes
      * visibility when the menu icon is clicked. This test
      * should have two expectations: does the menu display when
      * clicked and does it hide when clicked again.
@@ -79,10 +79,10 @@ $(function() {
     });
   });
 
-  /* TODO: Write a new test suite named "Initial Entries" */
+  /* test suite named "Initial Entries" */
   describe('Initial Entries', function() {
 
-    /* TODO: Write a test that ensures when the loadFeed
+    /* A test that ensures when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
      * Remember, loadFeed() is asynchronous so this test will require
@@ -98,13 +98,21 @@ $(function() {
       done();
     });
   });
-  /* TODO: Write a new test suite named "New Feed Selection" */
+  /* test suite named "New Feed Selection" */
   describe('New Feed Selection', function() {
-
-    /* TODO: Write a test that ensures when a new feed is loaded
+    /* A test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
      */
-
+    var firstFeedEntry = document.getElementsByClassName('entry')[0];
+    beforeEach(function(done) {
+      loadFeed(2, function() {
+        done();
+      });
+    });
+    it('should contain at least one entry', function(done) {
+      expect(document.getElementsByClassName('entry')[0]).not.toBe(firstFeedEntry);
+      done();
+    });
   });
 }());
